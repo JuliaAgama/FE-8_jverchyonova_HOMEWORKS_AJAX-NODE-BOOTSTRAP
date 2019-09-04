4550abc423b9927269e3de1314b553085a28a95b
Julia Ver <jverchyonova@gmail.com>
2019-09-04 10:32:19 +0300

    repository created

---
+++ AJAX_HW-001/js/script.js
@@ -0,0 +1,14 @@
+/*** Ajax. NodeJS. BOOTSTRAP. Lesson 04. BOOTSTRAP. Personal Exercise #01 ****/
+;
+/********** -- **********/
+
+
+console.log('*********');
+
+console.log('this is script.js')
+
+
+console.log('****************************************')
+
+
+
